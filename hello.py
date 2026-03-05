04a5813d45e3ec63524832fdc611a0ee2a60ff40
ashrrose <ashwebpaiges@gmail.com>
2026-03-05 12:53:42 -0500

    My first line

---
+++ hello.py
@@ -0,0 +1,3 @@
+print("Hello, there. I'm Ashley and I'm coding with Python. This is my first line of code with this new language. Wish me luck!")
+name = input("What is your name? ")
+print("Nice to meet you, " + name + "!")
